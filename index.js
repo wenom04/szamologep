b35c3af0fca2e0df723818b0835428a6ba44555b
wenom04 <wenom04@gmail.com>
2024-03-12 19:04:05 +0100

    z7

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const kijelzo = document.getElementById('kijelzo');
 
 
 function KiirAKijelzore(input){
-    if(kijelzo.value === 'Hiba')
+    if(kijelzo.value === 'Hiba' || kijelzo.value === 'infinity' || kijelzo.value === 'undefined')
         return;
     kijelzo.value += input;
+    kijelzo.scrollLeft = kijelzo.scrollWidth;
 }
 
 function TorliAKijelzot(){
@@ -36,3 +37,18 @@ function Backspace() {
     if(SzamE(kijelzo.value.slice(-1)))
         kijelzo.value = kijelzo.value.slice(0, -1);
 }
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if (!isNaN(key)) {
+        KiirAKijelzore(key);
+    } else if (key === 'Backspace' || key === 'Delete') { 
+        Backspace();
+    } else if (key === 'Shift') {
+        Kiszamol();
+    }else if (key === 'Escape') {
+        TorliAKijelzot();
+    } else if (key === '/' || key === '+' || key === '*' || key === '-') {
+        KiirAKijelzore(key);
+    }
+});
